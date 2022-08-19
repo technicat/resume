5f6c6f5e1b180e7a290e23f605d09ab5c0a70822
Philip Chu <technicat@users.noreply.github.com>
2022-08-18 18:40:09 -0700

    Update resume.scm

--- resume.scm
+++ resume.scm
@@ -10,19 +10,26 @@
  (let-args (cdr args)
   ((h "h|help" => (cut help (car args)))
    (f "f|file=s")
+   (o "o|out=s")
    (v "v|verbose")
    . restargs)
   (if (not h)
    (if f
-    (print (read-resume f))))))
+    (let ((e (read-json f)))
+     (if v (print e))
+     (if o (write-json o)))))))
 
 (define (help file)
  (print "Read JSON resume"))
 
-(define (read-resume file)
+(define (read-json file)
  (guard (e (else (print #"JSON error in ~file")
             (print (condition-message e))
             #\f))
+  ; assume one json obj, otherwise use parse-json*
   (let ((exp (call-with-input-file file parse-json)))
    exp)))
 
+(define (write-json file)
+ (call-with-output-file file
+  (lambda (out) (construct-json e out))))
