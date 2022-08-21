bce03797b4bffa93239eaca955805efa88016465
Philip Chu <technicat@users.noreply.github.com>
2022-08-20 17:47:28 -0700

    themes

---
+++ themes.scm
@@ -0,0 +1,17 @@
+#!/usr/local/bin/gosh
+
+(use gauche.parseopt)
+
+(define (main args)
+ (let-args (cdr args)
+  ((h "h|help" => (cut help (car args)))
+   (f "f|file=s")
+   (o "o|out=s")
+   (v "v|verbose")
+   . restargs)
+  (if (not h)
+    (sys-exec "resume" '("resume" "export" "even.html")))))
+
+(define (help file)
+ (print "Read/convert JSON resume"))
+
